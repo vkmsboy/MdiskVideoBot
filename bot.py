2d69d6216438da1e2e788b3c8b62d4e0ba7053ec
vkmsboy <95024188+vkmsboy@users.noreply.github.com>
2022-03-03 00:46:04 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ async def Doodstream_uploader(bot, message):
     try:
         Doodstream_link = await multi_Doodstream_up(new_string)
         await bot.delete_messages(chat_id=message.chat.id, message_ids=dele)
-        await message.reply(f'**{Doodstream_link} h1**' , quote=True)
+        await message.reply(f'**h1 {Doodstream_link}**' , quote=True)
     except Exception as e:
         await message.reply(f'Error: {e}', quote=True)
 
